4034eb71d17d0a961d0a74c86e74e62c6460c94e
medakshchoudhary <ch.daksh01@gmail.com>
2024-05-20 23:44:18 +0530

    updated the frontend a little more

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function LandingPage() {
   return (
-    <div className='w-full h-screen bg-zinc-900'>
+    // we write pt-1 here because the app.jsx is taking mt-32 as base and shifting the navbar as well to the bottom by mt-32 thereby adding pt-1 gives this a priority and the nav bar stays ixed on the top
+    
+    // or we can undertand by that after adding pt-1 we have the first element as the main outer div so the navbar takes it as refrence for position rather than taking the textstructure div as refrence with m-32
+    <div className='w-full h-screen bg-zinc-900 pt-1'>
         <div className='textstructure mt-32'>
             <div className='masker'>
                 <h1 className='text-9xl font-foundersgrotesk'>WE CREATE</h1>
